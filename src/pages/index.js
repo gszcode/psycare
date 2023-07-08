ff807637243378e0bf6e310261aa988c7b798466
Gabriel <gszcode@gmail.com>
2023-07-08 03:10:40 -0300

    Initial commit from Create Next App

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          <h1>Bienvenido a PsyCare</h1>
+          <p>
+            Tu lugar de confianza para el bienestar mental y equilibrio
+            emocional.
+          </p>
+          <button>¡Agenda tu cita ahora!</button>
+        </section>
+      </main>
+      {/* Agrega más secciones según sea necesario */}
+    </>
+  )
+}
