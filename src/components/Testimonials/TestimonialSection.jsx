5c2276fcf89a7a4a5cad55ecd925e7925a0326f7
Gabriel <gszcode@gmail.com>
2023-07-11 23:19:35 -0300

    refactorizacion de testimonios

--- src/components/Testimonials/TestimonialSection.jsx
+++ src/components/Testimonials/TestimonialSection.jsx
@@ -1,70 +1,34 @@
 import { FiStar } from 'react-icons/fi'
 import styles from './TestimonialSection.module.css'
+import { testimonialsData } from '../../utils/data'
 
 const TestimonialSection = () => {
+  const starsCount = 4
+
+  const renderStars = () => {
+    const stars = []
+
+    for (let i = 1; i <= starsCount; i++) {
+      stars.push(<FiStar key={i} className={styles.starIcon} />)
+    }
+
+    return stars
+  }
+
   return (
     <section className={styles.testimonialsSection} id="testimonials">
       <div className={styles.container}>
         <h2 className={styles.title}>Testimonios</h2>
         <div className={styles.testimonials}>
-          <div className={styles.testimonial}>
-            <div className={styles.rating}>
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-            </div>
-            <p className={styles.testimonialText}>
-              ¡La terapia en PsyCare ha sido transformadora! Me ha ayudado a
-              entender y superar mis miedos. ¡Gracias por su apoyo y
-              profesionalidad!
-            </p>
-            <p className={styles.clientName}>- Ana Gómez</p>
-          </div>
-          <div className={styles.testimonial}>
-            <div className={styles.rating}>
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-            </div>
-            <p className={styles.testimonialText}>
-              Recomiendo a PsyCare sin ninguna duda. Su enfoque personalizado y
-              su trato cálido me han hecho sentir escuchado y comprendido
-              durante todo el proceso de terapia.
-            </p>
-            <p className={styles.clientName}>- Carlos Martínez</p>
-          </div>
-          <div className={styles.testimonial}>
-            <div className={styles.rating}>
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-            </div>
-            <p className={styles.testimonialText}>
-              Mi experiencia en PsyCare ha sido excepcional. Los profesionales
-              son altamente cualificados y han sabido brindarme las herramientas
-              necesarias para afrontar mis desafíos emocionales.
-            </p>
-            <p className={styles.clientName}>- Laura Rodríguez</p>
-          </div>
-          <div className={styles.testimonial}>
-            <div className={styles.rating}>
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-              <FiStar className={styles.starIcon} />
-            </div>
-            <p className={styles.testimonialText}>
-              Estoy muy agradecido por la ayuda y el apoyo que he recibido en
-              PsyCare. Gracias a su profesionalismo y dedicación, he logrado un
-              gran progreso en mi bienestar emocional.
-            </p>
-            <p className={styles.clientName}>- Pedro Sánchez</p>
-          </div>
+          {testimonialsData.map((item) => {
+            return (
+              <div key={item.id} className={styles.testimonial}>
+                <div className={styles.rating}>{renderStars()}</div>
+                <p className={styles.testimonialText}>{item.description}</p>
+                <p className={styles.clientName}>- {item.name}</p>
+              </div>
+            )
+          })}
         </div>
         {/* <button className={styles.viewMoreButton}>Ver más testimonios</button> */}
       </div>
