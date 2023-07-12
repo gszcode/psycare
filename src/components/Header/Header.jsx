93f1e4efa2b7f38d1cd6c8f580f520a684d9565f
Gabriel <gszcode@gmail.com>
2023-07-11 23:01:45 -0300

    refactorizacion del navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import styles from './Header.module.css'
+import { headerLinks } from '../../utils/data'
 
 const Header = () => {
   return (
@@ -6,24 +7,11 @@ const Header = () => {
       <h1 className={styles.title}>PsyCare</h1>
       <nav className={styles.nav}>
         <ul className={styles.navList}>
-          <li className={styles.navItem}>
-            <a href="#home">Inicio</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="#about">Nosotros</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="#services">Servicios</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="#focus">Enfoque</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="#testimonials">Testimonios</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="#contact">Contacto</a>
-          </li>
+          {headerLinks.map((link) => (
+            <li key={link.id} className={styles.navItem}>
+              <a href={`#${link.href}`}>{link.link}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
