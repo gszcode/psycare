2bc7a7cdc56d1afcef8a90f3f6eb449b59b14b9e
Gabriel <gszcode@gmail.com>
2023-07-11 23:36:09 -0300

    data de servicios agregada a utilidades para refactorizacion

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,28 +1,24 @@
 export const focusData = [
   {
     id: 1,
-    icon: 'FiTarget',
     title: 'Enfoque Personalizado',
     description:
       'Adaptamos nuestras terapias para satisfacer tus necesidades individuales y brindarte el apoyo adecuado.'
   },
   {
     id: 2,
-    icon: 'FiBookOpen',
     title: 'Terapias Basadas en Evidencia',
     description:
       'Utilizamos métodos y técnicas respaldados por la investigación para garantizar la efectividad de nuestras terapias.'
   },
   {
     id: 3,
-    icon: 'FiUsers',
     title: 'Enfoque de Equipo',
     description:
       'Trabajamos en colaboración con otros profesionales de la salud para brindarte un cuidado integral y multidisciplinario.'
   },
   {
     id: 4,
-    icon: 'FiHeart',
     title: 'Compromiso Empático',
     description:
       ' Nos dedicamos a establecer una relación de confianza y empatía contigo para que te sientas cómodo y apoyado en todo momento.'
@@ -86,5 +82,32 @@ export const testimonialsData = [
     description:
       'Estoy muy agradecido por la ayuda y el apoyo que he recibido en PsyCare. Gracias a su profesionalismo y dedicación, he logrado un gran progreso en mi bienestar emocional.',
     name: 'Pedro Sánchez'
+  }
+]
+
+export const servicesData = [
+  {
+    id: 1,
+    title: 'Apoyo Emocional',
+    description:
+      'Brindamos un espacio seguro y confidencial para ayudarte a gestionar tus emociones y superar desafíos emocionales.'
+  },
+  {
+    id: 2,
+    title: 'Terapia Individual',
+    description:
+      'Ofrecemos terapia individual personalizada para abordar tus necesidades específicas y promover tu bienestar mental.'
+  },
+  {
+    id: 3,
+    title: 'Terapia de Grupo',
+    description:
+      'Facilitamos sesiones de terapia de grupo donde puedes conectarte y compartir experiencias con personas afines.'
+  },
+  {
+    id: 4,
+    title: 'Crecimiento Personal',
+    description:
+      'Te ayudamos a desarrollar habilidades de afrontamiento y fomentar un crecimiento personal significativo.'
   }
 ]
