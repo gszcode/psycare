fcd7b8d00a6180009d2e6eee47184632f6a71262
Gabriel <gszcode@gmail.com>
2023-07-11 23:36:22 -0300

    refactorizacion de codigo

--- src/components/Focus/FocusSection.jsx
+++ src/components/Focus/FocusSection.jsx
@@ -3,6 +3,13 @@ import { FiTarget, FiBookOpen, FiUsers, FiHeart } from 'react-icons/fi'
 import { focusData } from '../../utils/data'
 
 const FocusSection = () => {
+  const renderIcons = (id) => {
+    if (id === 1) return <FiTarget className={styles.icon} />
+    if (id === 2) return <FiBookOpen className={styles.icon} />
+    if (id === 3) return <FiUsers className={styles.icon} />
+    if (id === 4) return <FiHeart className={styles.icon} />
+  }
+
   return (
     <section className={styles.focusSection} id="focus">
       <div className={styles.container}>
@@ -10,12 +17,7 @@ const FocusSection = () => {
         <div className={styles.focusAreas}>
           {focusData.map((item) => (
             <div key={item.id} className={styles.focusArea}>
-              {item.icon === 'FiTarget' && <FiTarget className={styles.icon} />}
-              {item.icon === 'FiBookOpen' && (
-                <FiBookOpen className={styles.icon} />
-              )}
-              {item.icon === 'FiUsers' && <FiUsers className={styles.icon} />}
-              {item.icon === 'FiHeart' && <FiHeart className={styles.icon} />}
+              {renderIcons(item.id)}
               <h3 className={styles.focusAreaTitle}>{item.title}</h3>
               <p className={styles.focusAreaDescription}>{item.description}</p>
             </div>
--- src/components/Services/ServiceSection.jsx
+++ src/components/Services/ServiceSection.jsx
@@ -1,44 +1,29 @@
 import { FiHeart, FiUsers, FiActivity, FiSmile } from 'react-icons/fi'
 import styles from './ServiceSection.module.css'
+import { servicesData } from '../../utils/data'
 
 const ServiceSection = () => {
+  const renderIcons = (id) => {
+    if (id === 1) return <FiHeart className={styles.icon} />
+    if (id === 2) return <FiUsers className={styles.icon} />
+    if (id === 3) return <FiActivity className={styles.icon} />
+    if (id === 4) return <FiSmile className={styles.icon} />
+  }
+
   return (
     <section className={styles.servicesSection} id="services">
       <div className={styles.container}>
         <h2 className={styles.title}>Nuestros Servicios</h2>
         <div className={styles.services}>
-          <div className={styles.service}>
-            <FiHeart className={styles.icon} />
-            <h3 className={styles.serviceTitle}>Apoyo Emocional</h3>
-            <p className={styles.serviceDescription}>
-              Brindamos un espacio seguro y confidencial para ayudarte a
-              gestionar tus emociones y superar desafíos emocionales.
-            </p>
-          </div>
-          <div className={styles.service}>
-            <FiUsers className={styles.icon} />
-            <h3 className={styles.serviceTitle}>Terapia Individual</h3>
-            <p className={styles.serviceDescription}>
-              Ofrecemos terapia individual personalizada para abordar tus
-              necesidades específicas y promover tu bienestar mental.
-            </p>
-          </div>
-          <div className={styles.service}>
-            <FiActivity className={styles.icon} />
-            <h3 className={styles.serviceTitle}>Terapia de Grupo</h3>
-            <p className={styles.serviceDescription}>
-              Facilitamos sesiones de terapia de grupo donde puedes conectarte y
-              compartir experiencias con personas afines.
-            </p>
-          </div>
-          <div className={styles.service}>
-            <FiSmile className={styles.icon} />
-            <h3 className={styles.serviceTitle}>Crecimiento Personal</h3>
-            <p className={styles.serviceDescription}>
-              Te ayudamos a desarrollar habilidades de afrontamiento y fomentar
-              un crecimiento personal significativo.
-            </p>
-          </div>
+          {servicesData.map((item) => {
+            return (
+              <div key={item.id} className={styles.service}>
+                {renderIcons(item.id)}
+                <h3 className={styles.serviceTitle}>{item.title}</h3>
+                <p className={styles.serviceDescription}>{item.description}</p>
+              </div>
+            )
+          })}
         </div>
       </div>
     </section>
