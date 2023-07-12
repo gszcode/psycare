f10afc0007fd093d6bc9310a045b02596be5143b
Gabriel <gszcode@gmail.com>
2023-07-11 21:46:24 -0300

    componente de servicios agregado

---
+++ src/components/Services/ServiceSection.jsx
@@ -0,0 +1,48 @@
+import { FiHeart, FiUsers, FiActivity, FiSmile } from 'react-icons/fi'
+import styles from './ServiceSection.module.css'
+
+const ServiceSection = () => {
+  return (
+    <section className={styles.servicesSection}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Nuestros Servicios</h2>
+        <div className={styles.services}>
+          <div className={styles.service}>
+            <FiHeart className={styles.icon} />
+            <h3 className={styles.serviceTitle}>Apoyo Emocional</h3>
+            <p className={styles.serviceDescription}>
+              Brindamos un espacio seguro y confidencial para ayudarte a
+              gestionar tus emociones y superar desafíos emocionales.
+            </p>
+          </div>
+          <div className={styles.service}>
+            <FiUsers className={styles.icon} />
+            <h3 className={styles.serviceTitle}>Terapia Individual</h3>
+            <p className={styles.serviceDescription}>
+              Ofrecemos terapia individual personalizada para abordar tus
+              necesidades específicas y promover tu bienestar mental.
+            </p>
+          </div>
+          <div className={styles.service}>
+            <FiActivity className={styles.icon} />
+            <h3 className={styles.serviceTitle}>Terapia de Grupo</h3>
+            <p className={styles.serviceDescription}>
+              Facilitamos sesiones de terapia de grupo donde puedes conectarte y
+              compartir experiencias con personas afines.
+            </p>
+          </div>
+          <div className={styles.service}>
+            <FiSmile className={styles.icon} />
+            <h3 className={styles.serviceTitle}>Crecimiento Personal</h3>
+            <p className={styles.serviceDescription}>
+              Te ayudamos a desarrollar habilidades de afrontamiento y fomentar
+              un crecimiento personal significativo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ServiceSection
