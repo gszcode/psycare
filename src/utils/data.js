061265532fdd15899912f23c120aa6b287c6c5bf
Gabriel <gszcode@gmail.com>
2023-07-11 22:57:05 -0300

    utilidades agregado para datos refactorizados

---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+export const focusData = [
+  {
+    id: 1,
+    icon: 'FiTarget',
+    title: 'Enfoque Personalizado',
+    description:
+      'Adaptamos nuestras terapias para satisfacer tus necesidades individuales y brindarte el apoyo adecuado.'
+  },
+  {
+    id: 2,
+    icon: 'FiBookOpen',
+    title: 'Terapias Basadas en Evidencia',
+    description:
+      'Utilizamos métodos y técnicas respaldados por la investigación para garantizar la efectividad de nuestras terapias.'
+  },
+  {
+    id: 3,
+    icon: 'FiUsers',
+    title: 'Enfoque de Equipo',
+    description:
+      'Trabajamos en colaboración con otros profesionales de la salud para brindarte un cuidado integral y multidisciplinario.'
+  },
+  {
+    id: 4,
+    icon: 'FiHeart',
+    title: 'Compromiso Empático',
+    description:
+      ' Nos dedicamos a establecer una relación de confianza y empatía contigo para que te sientas cómodo y apoyado en todo momento.'
+  }
+]
