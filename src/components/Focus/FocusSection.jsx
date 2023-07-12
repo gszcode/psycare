c3dfab5bfe01a85ecb3ed6d058c309178f71053f
Gabriel <gszcode@gmail.com>
2023-07-11 22:56:44 -0300

    refactorizacion de codigo

--- src/components/Focus/FocusSection.jsx
+++ src/components/Focus/FocusSection.jsx
@@ -1,5 +1,6 @@
-import { FiTarget, FiBookOpen, FiUsers, FiHeart } from 'react-icons/fi'
 import styles from './FocusSection.module.css'
+import { FiTarget, FiBookOpen, FiUsers, FiHeart } from 'react-icons/fi'
+import { focusData } from '../../utils/data'
 
 const FocusSection = () => {
   return (
@@ -7,40 +8,18 @@ const FocusSection = () => {
       <div className={styles.container}>
         <h2 className={styles.title}>Nuestro Enfoque</h2>
         <div className={styles.focusAreas}>
-          <div className={styles.focusArea}>
-            <FiTarget className={styles.icon} />
-            <h3 className={styles.focusAreaTitle}>Enfoque Personalizado</h3>
-            <p className={styles.focusAreaDescription}>
-              Adaptamos nuestras terapias para satisfacer tus necesidades
-              individuales y brindarte el apoyo adecuado.
-            </p>
-          </div>
-          <div className={styles.focusArea}>
-            <FiBookOpen className={styles.icon} />
-            <h3 className={styles.focusAreaTitle}>
-              Terapias Basadas en Evidencia
-            </h3>
-            <p className={styles.focusAreaDescription}>
-              Utilizamos métodos y técnicas respaldados por la investigación
-              para garantizar la efectividad de nuestras terapias.
-            </p>
-          </div>
-          <div className={styles.focusArea}>
-            <FiUsers className={styles.icon} />
-            <h3 className={styles.focusAreaTitle}>Enfoque de Equipo</h3>
-            <p className={styles.focusAreaDescription}>
-              Trabajamos en colaboración con otros profesionales de la salud
-              para brindarte un cuidado integral y multidisciplinario.
-            </p>
-          </div>
-          <div className={styles.focusArea}>
-            <FiHeart className={styles.icon} />
-            <h3 className={styles.focusAreaTitle}>Compromiso Empático</h3>
-            <p className={styles.focusAreaDescription}>
-              Nos dedicamos a establecer una relación de confianza y empatía
-              contigo para que te sientas cómodo y apoyado en todo momento.
-            </p>
-          </div>
+          {focusData.map((item) => (
+            <div key={item.id} className={styles.focusArea}>
+              {item.icon === 'FiTarget' && <FiTarget className={styles.icon} />}
+              {item.icon === 'FiBookOpen' && (
+                <FiBookOpen className={styles.icon} />
+              )}
+              {item.icon === 'FiUsers' && <FiUsers className={styles.icon} />}
+              {item.icon === 'FiHeart' && <FiHeart className={styles.icon} />}
+              <h3 className={styles.focusAreaTitle}>{item.title}</h3>
+              <p className={styles.focusAreaDescription}>{item.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
